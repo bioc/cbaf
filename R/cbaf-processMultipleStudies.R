13efbdffc815167cb6d16bb8707aa74d7211b81d
armanshahrisa <shahrisa.arman@hotmail.com>
2019-02-23 14:26:22 +0330

    - Fixing a minor bug

--- R/cbaf-processMultipleStudies.R
+++ R/cbaf-processMultipleStudies.R
@@ -12,8 +12,8 @@
 #' \tabular{lllll}{
 #' Package: \tab cbaf \cr
 #' Type: \tab Package \cr
-#' Version: \tab 1.4.0 \cr
-#' Date: \tab 2018-10-30 \cr
+#' Version: \tab 1.4.1 \cr
+#' Date: \tab 2019-02-22 \cr
 #' License: \tab Artistic-2.0 \cr
 #' }
 #'
